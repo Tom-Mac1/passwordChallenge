91d9cd750e2e53854d79e00d643b6c1c06777f49
Thomas Machin <thomas.machin@fujitsu.com>
2024-02-28 11:55:25 +0000

    button work

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,6 +1,8 @@
 /* Need to be able to set length, and choose certain characters to exclude (upper, lower, special, numerical)*/
 
-function Pass() {
+import { useState } from "react";
+
+function Pass(length) {
     var password = "";
     var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lower = "abcdefghijklmnopqrstuvwxyz";
@@ -9,27 +11,35 @@ function Pass() {
     var characters = upper + lower + nums + special;
     /* create useffect for upperselect, lowerselect, nums and join them into one list based on whats true */
     /* create one for count with an up/down toggle */
-    for (var i=0; i < 20; i++){
+    for (var i=0; i < length; i++){
         password += characters.charAt(Math.floor(Math.random() * characters.length))
     }
     return password
 }
 function Hero() {
-    
+    const [upper, setUpper] = useState("true");
+    const [lower, setLower] = useState("true");
+    const [numbers, setNumbers] = useState("true");
+    const [special, setSpecial] = useState("true");
+    const [length, setLength] = useState(0);
     return(
         <div className="HeroPage">
             <div className="Options">
-                <button>UPPER</button>
+                <button onClick={() => upper === "true" ? setUpper("false") : setUpper("true")}>UPPER: {upper}</button>
                 <br/>
-                <button>lower</button>
+                <button onClick={() => lower === "true" ? setLower("false") : setLower("true")}>lower: {lower}</button>
                 <br/>
-                <button>Numbers</button>
+                <button onClick={() => numbers === "true" ? setNumbers("false") : setNumbers("true")}>Numbers: {numbers}</button>
                 <br/>
-                <button>Special</button>
+                <button onClick={() => special === "true" ? setSpecial("false") : setSpecial("true")}>Special: {special}</button>
+                <br/>
+                <p>Length: {length}</p>
+                <button onClick={() => length === 0 ? setLength(length) : setLength(length-1)}>-</button> 
+                <button onClick={() => setLength(length+1)}>+</button>
             </div>
             <div className="selector">
                 <button>Generate Password</button>
-                <p>{Pass()}</p>
+                <p>{Pass(length)}</p>
             </div>
         </div>
     )
